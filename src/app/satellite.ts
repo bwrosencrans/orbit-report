24cc95560001c875eddcf91772f9ba531750de55
Brad Rosencrans <brad.rosencrans@outlook.com>
2019-11-03 01:52:52 -0500

    changes

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,23 @@
 export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+
+    }
+
+    public shouldWarning(): boolean {
+        if(this.type.toLocaleLowerCase() === 'space debris')
+        return true;
+        if(this.type != 'space debris')
+        return false;
+    }
 }
